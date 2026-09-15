cb658897526696659cafae56796790b5bb71bcec
hasanmoriya <developer.hasan429@gmail.com>
2026-09-15 16:15:28 +0530

    improvements..

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        {{-- Google Analytics (GA4) --}}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JMYVZCVX83"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JMYVZCVX83');
+        </script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
